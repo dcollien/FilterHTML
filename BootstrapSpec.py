35489a0b9c18e92a263b91b40de9d91d33a9fb9e
David Collien <david@icy.com.au>
2013-02-18 12:58:45 +1100

    Added a bootstrap-themed HTML whitelist specification
    This bootstrap whitelist spec contains many of the tags and classes
    needed to render Bootstrap 2.3.0 elements that would commonly be
    required in page content.

---
+++ BootstrapSpec.py
@@ -0,0 +1,240 @@
+HTML_WHITELIST = {
+	'div': {
+		'class': [
+			'well',
+			'well-small',
+			'well-large',
+
+			'pull-right',
+			'pull-left',
+			'clearfix',
+
+			'media',
+			'media-body',
+
+			'page-header',
+			'hero-unit',
+
+			'alert',
+			'alert-block',
+			'alert-error',
+			'alert-success',
+			'alert-info',
+
+			'thumbnail'
+		]
+	},
+
+	'span': {
+		'class': [
+			'badge',
+			'badge-success',
+			'badge-warning',
+			'badge-important',
+			'badge-info',
+			'badge-inverse',
+			
+			'label',
+			'label-success',
+			'label-warning',
+			'label-important',
+			'label-info',
+			'label-inverse'
+		]
+	},
+
+	'h1': {
+		'class': [
+			'media-heading'
+		]
+	},
+	'h2': {
+		'class': [
+			'media-heading'
+		]
+	},
+	'h3': {
+		'class': [
+			'media-heading'
+		]
+	},
+	'h4': {
+		'class': [
+			'media-heading'
+		]
+	},
+	'h5': {
+		'class': [
+			'media-heading'
+		]
+	},
+	'h6': {
+		'class': [
+			'media-heading'
+		]
+	},
+
+	'a': {
+		'href': 'url',
+		'target': [
+			'_blank'
+		],
+		'class': [
+			'thumbnail',
+
+			'pull-right',
+			'pull-left',
+			'clearfix',
+
+			'btn',
+			'btn-primary',
+			'btn-info',
+			'btn-success',
+			'btn-warning',
+			'btn-danger',
+			'btn-inverse',
+			'btn-large',
+			'btn-small',
+			'btn-mini',
+			'btn-block',
+			'disabled'
+		]
+	},
+
+	'img': {
+		'src': 'url',
+		'alt': re.compile(r'^[\w\s]+$'),
+		'width': re.compile(r'^\d+(px)*$'),
+		'height': re.compile(r'^\d+(px)*$'),
+		'class': [
+			'media-object',
+			'img-rounded',
+			'img-circle',
+			'img-polaroid'
+		]
+	},
+
+	'hr': {
+		'class': [
+			'clearfix'
+		]
+	},
+	'br': {
+		'class': [
+			'clearfix'
+		]
+	},
+	'b': {},
+	'strong': {},
+	'small': {},
+	'em': {},
+	'i': {
+		'class': re.compile(r'^icon-[a-z0-9_]+$')
+	},
+
+	'abbr': {
+		'title': re.compile(r'^[\w\s]+$'),
+		'class': [
+			'initialism'
+		]
+	},
+	'address': {},
+	'blockquote': {
+		'class': [
+			'pull-right'
+		]
+	},
+	'cite': {
+		'title': re.compile(r'^[\w\s]+$')
+	},
+
+	'li': {
+		'class': [
+			'span1',
+			'span2',
+			'span3',
+			'span4',
+			'span5',
+			'span6',
+			'span7',
+			'span8',
+			'span9',
+			'span10',
+			'span11',
+			'span12'
+		]
+	},
+	'ul': {
+		'class': [
+			'unstyled',
+			'inline',
+
+			'pager',
+			'thumbnails',
+			'media-list'
+		]
+	},
+	'ol': {
+		'start': 'int',
+		'type': [
+			'1',
+			'A',
+			'a',
+			'I',
+			'i'
+		]
+	},
+	'dl': {
+		'class': [
+			'dl-horizontal'
+		]
+	},
+
+	'code': {},
+	'pre': {
+		'class': [
+			'pre-scrollable'
+		]
+	},
+
+	'table': {
+		'class': [
+			'table',
+			'table-striped',
+			'table-bordered',
+			'table-hover',
+			'table-condensed'
+		]
+	},
+	'thead': {},
+	'tbody': {},
+	'tfoot': {},
+	'tr': {
+		'class': [
+			'success',
+			'error',
+			'warning',
+			'info'
+		]
+	},
+	'td': {},
+	'caption': {},
+
+	'p': {
+		'class': [
+			'text-left',
+			'text-center',
+			'text-right',
+			'muted',
+			'text-warning',
+			'text-error',
+			'text-info',
+			'text-success',
+			'lead'
+		]
+	},
+
+	# aliases
+	'center': 'p class="text-center"',
+	'script': 'code'
+}
