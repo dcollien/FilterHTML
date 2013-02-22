0d7dd57515fac6c567e6091e33a08d199e47c91d
John Garland <johnnybg@gmail.com>
2013-02-22 17:29:01 +1100

    Make repo into a python module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "FilterHTML",
+    version = "0.1",
+    packages = find_packages(),
+    license = "MIT",
+    author = "David Collien",
+    url = "http://github.com/dcollien/FilterHTML",
+)
