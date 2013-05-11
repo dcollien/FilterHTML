65eb795e03e10b4b7e5a6280a85376e683f68f6a
David Collien <david@openlearning.com>
2013-05-11 22:22:42 +1000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='FilterHTML',
-   version='0.0.7',
+   version='0.0.8',
    py_modules=['FilterHTML'],
    author='David Collien',
    author_email='me@dcollien.com',
